00ea895e7186cb4d18208c26509b812f2f0e2d9b
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-11-24 13:06:25 +0100

    Add data pull push scripts

---
+++ pull-data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+path=$1; shift
+dataset=$1; shift
+
+function usage() {
+    echo "ERROR: wrong arguments"
+    echo "usage:"
+    echo "pull-data.sh LOCAL_PATH DATASET_FOLDER"
+}
+
+if [ -z ${path} ]; then usage; exit 1; fi
+if [ -z ${dataset} ]; then usage; exit 1; fi
+
+rsync -r "dl25e23@gpucluster.st.lab.au.dk:~/datasets/${dataset}/**" "${path}/"
