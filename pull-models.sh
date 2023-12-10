0313a05060cd435170c899f3952aaadb21abc6f1
Lorenzo Panieri <panierilorenzo@gmail.com>
2023-12-10 14:34:06 +0100

    feat: make pull-models less terrible

--- pull-models.sh
+++ pull-models.sh
@@ -1,11 +1,18 @@
 model_name=$1; shift
+epoch=$1; shift
 
 function usage() {
     echo "ERROR: wrong arguments"
     echo "usage:"
-    echo "pull-data.sh MODEL_NAME"
+    echo "pull-data.sh MODEL_NAME EPOCH"
 }
 
 if [ -z ${model_name} ]; then usage; exit 1; fi
+if [ -z ${epoch} ]; then usage; exit 1; fi
 
-scp -r dl25e23@gpucluster.st.lab.au.dk:deep-learning-final-project/models/${model_name}/* "models/${model_name}/"
+model_full_name="${model_name}/epoch_${epoch}"
+remote_path="dl25e23@gpucluster.st.lab.au.dk:deep-learning-final-project/models/${model_full_name}/*"
+local_path="models/${model_full_name}/"
+
+mkdir -p "$local_path"
+scp -r "$remote_path" "$local_path"
